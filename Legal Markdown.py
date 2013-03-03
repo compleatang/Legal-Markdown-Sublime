15b48e73981577f26ae7441b1908098f3543ebda
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2013-03-03 12:39:25 +0300

    Add Unique Clauses as its own category && guard against building YAML when there is nothing to build.

--- Legal Markdown.py
+++ Legal Markdown.py
@@ -10,7 +10,8 @@ class BuildYamlFrontMatter(sublime_plugin.TextCommand):
       self.view.replace(edit, selection, result)
 
   def buildYamlFrontMatter( self, selection ):
-    mixins_pattern = re.compile("\{\{(\S+)\}\}")
+    clauses_pattern = re.compile("\[\{\{(\S+)\}\}")
+    mixins_pattern = re.compile("\{\{(\S+)\}\}\s")
     headers_pattern = re.compile("^(l+)\.\s",re.MULTILINE)
     yaml_pattern = re.compile("^(\s*---.*---\s*$)",re.MULTILINE|re.DOTALL)
 
@@ -18,19 +19,37 @@ class BuildYamlFrontMatter(sublime_plugin.TextCommand):
     if yaml:
       selection = selection.replace( yaml[0], '' )
 
+    clauses = clauses_pattern.findall( selection )
+    uniq_clauses  = [list(x) for x in set(tuple(x) for x in clauses)]
+    uniq_clauses.sort()
+    
     mixins = mixins_pattern.findall( selection )
-    # uniq_mixins  = list(set(mixins)).sort()
     uniq_mixins  = [list(x) for x in set(tuple(x) for x in mixins)]
     uniq_mixins.sort()
+
     headers = headers_pattern.findall( selection )
-    # uniq_headers = list(set(headers)).sort()
     uniq_headers = [list(x) for x in set(tuple(x) for x in headers)]
     uniq_headers.sort()
-    new_yaml =  '---\n\n# First, the Mixins\n'
+
+    if len(uniq_clauses) == 0 and len(uniq_mixins) == 0 and len(uniq_headers) == 0:
+      return selection
+
+    new_yaml =  '---\n'
+
+    if len(uniq_clauses) != 0:
+      new_yaml = new_yaml + '\n# Optional Clauses\n'
+    for clause in uniq_clauses:
+      clause = ''.join(clause)
+      new_yaml = new_yaml + clause + ': \n'
+
+    if len(uniq_mixins) != 0:
+      new_yaml = new_yaml + '\n# Mixins\n'
     for mixin in uniq_mixins:
       mixin = ''.join(mixin)
       new_yaml = new_yaml + mixin + ': \n'
-    new_yaml = new_yaml + '# Now, for the Headers\n'
+
+    if len(uniq_headers) != 0:
+      new_yaml = new_yaml + '\n# Structured Headers\n'
     for header in uniq_headers:
       new_yaml = new_yaml + 'level-' + str(header.count('l')) + ': \n'
     new_yaml = new_yaml + '\n---\n'
